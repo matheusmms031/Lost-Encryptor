e713154cdd05ad24326930cf4e6b801f1b1bb2e1
TeuzimMG <basemagalha@gmail.com>
2024-01-02 15:43:37 -0300

    Create a function asciiPlus and create param data,returnType and respose a class LostEncrypt

--- lost-encryptor/__init__.py
+++ lost-encryptor/__init__.py
@@ -1,8 +1,37 @@
 class LostEncrypt():
 
-    def __init__(self, data):
+    def __init__(self, data, returnType = 'str'):
         self.data = data
+        self.respose = data
+        self.returnType = returnType
+
+
+    def asciiPlus(self,n: int = 1, incrementFor: int = 0):
+        match self.returnType:
+            case 'str':
+                response = ''
+                if incrementFor != 0:
+                    for letter in self.data:
+                        response += chr(ord(letter)+n)
+                        n += incrementFor
+                else:
+                    for letter in self.data:
+                        response += chr(ord(letter)+n)
+            case 'ascii':
+                response = []
+                if incrementFor != 0:
+                    for letter in self.data:
+                        response.append(ord(letter)+n)
+                        n += incrementFor
+                else:
+                    for letter in self.data:
+                        response.append(ord(letter)+n)
+        self.response = response
+        return self.response
+
+
+
     
     
-    def asciiPlus(self,n: int):
-        
+enc = LostEncrypt('teste','str')
+print(enc.asciiPlus(1,1))
