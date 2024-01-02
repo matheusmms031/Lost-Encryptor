6c8ea9a3e0860ecacef345a9f74a411f7b160ee5
TeuzimMG <basemagalha@gmail.com>
2024-01-02 19:40:00 -0300

    Add tests files

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,11 @@
-from ascii import AsciiEncrypt
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+print(sys.path)
+
+
+from src.ascii import AsciiEncrypt
 
 
 # The `LostEncrypt` class is a subclass of `AsciiEncrypt` that encrypts data using ASCII values and
@@ -7,7 +14,6 @@ class LostEncrypt(AsciiEncrypt):
 
     def __init__(self, data, returnType:str = 'str', asciiTableInterval = [33,126]):
         AsciiEncrypt.__init__(self,asciiTableInterval)
-        
         
         self.data = data
         self.returnType = returnType
