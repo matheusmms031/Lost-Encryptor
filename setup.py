163c44e589691174fd09f3321b232417102b8f72
TeuzimMG <basemagalha@gmail.com>
2024-01-03 08:07:07 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name='Lost-Encryptor',
+    packages=['src','src.ascii'],
+    version='0.1beta',
+    requires=['numpy']
+)
--- tests/test_ascii.py
+++ tests/test_ascii.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 import pytest
+from pytest import mark
 
 
 
@@ -9,9 +10,13 @@ sys.path.append(str(Path(__file__).parent.parent))
 from src import LostEncrypt
 
 
-
-
-def test_mytest():
-    env = LostEncrypt("ABC")
-    assert env.data == 'ABC'
+@mark.asciiPlus
+@mark.parametrize(
+    'entrada,esperado,parametro',
+    [('ABCD','BCDE',1),('abcd','bcde',1),('ABCD','CDEF',2),('abcd','cdef',2),('ABCD','DEFG',3),('abcd','defg',3)]
+)
+def test_asciiPlus_parametro123_sem_incrementFor(entrada,esperado,parametro):
+    env = LostEncrypt(entrada)
+    env.asciiPlus(parametro)
+    assert env.data == esperado
 
